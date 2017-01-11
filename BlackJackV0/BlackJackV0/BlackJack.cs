a4500e6356bd299e21b5f98afcd717c479e1a0d5
K8455 <K8455@ICT-421-WS-39>
2017-01-11 15:07:25 +0200

    Lotto and BlackJack done next step: make them better

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -1,34 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 /// <summary>
 /// A simple example of Blackjack card game
 /// </summary>
 namespace JAMK.IT
 {
-  class BlackJack
-  {
-    static void Main()
+
+    class BlackJack
     {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
+
+        public static void PlayBlackJack()
         {
-          System.Console.WriteLine("You win.");
+
+            Random rand = new Random();
+            bool looping = true;
+            int myNumber = rand.Next(1, 21); 
+            int theirNumber;
+
+            System.Console.WriteLine("*** BlackJack! ***");
+            System.Console.Write("Can you beat my number? Enter any number between 1-21: (x and exit quits the game)");
+            //reading and converting   
+            theirNumber = 0;
+            //comparing that given umber is valid
+            List<int> userInts = new List<int>();
+
+            while (looping==true)
+            {
+
+                for (int i = 0; i < 1; i++)
+                {
+                    string userValue = Console.ReadLine();
+                    if (userValue == "x" || userValue == "exit") looping = false;
+                    int userInt;
+                    if (int.TryParse(userValue, out userInt))
+                    {
+                        userInts.Add(userInt);
+                        theirNumber += userInt;
+                        Console.Write(theirNumber+"\n");
+                    }
+                }
+
+                if (theirNumber > 21)
+                {
+                    Console.WriteLine("Im sorry you lose ");
+                    theirNumber = 0;
+                    myNumber = rand.Next(10, 21);
+
+                }
+                if (theirNumber > myNumber)
+                {
+                    Console.WriteLine("YOU WIN! Dealer had: " + myNumber);
+                    theirNumber = 0;
+                    
+                    myNumber = rand.Next(10, 21);
+                }
+                else
+                {
+                    Console.WriteLine("You need to take new card");
+                }
+
+
+
+
+            }
         }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
     }
-  }
 }
---
+++ BlackJackV0/BlackJackV0/Lotto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+//
+namespace JAMK.IT
+{
+    class Lottery
+    {
+        
+        public static void PlayLotto()
+        {
+
+            Random rand = new Random();
+            const int LOTTO = 7;
+            int[] Numbers = new int[LOTTO];
+            int temp = 0;
+            int[] Winning = new int[LOTTO];
+
+            Console.Write("How many lines you want to make? ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            //mainloop looppaa niin monesti kun käyttäjä haluaa
+            for (int i = 0; i < userNumber; i++)
+            {
+                for (int j = 0; j < LOTTO; j++)
+                {
+
+                    do
+                    {
+                        temp = rand.Next(1, 40);
+                    } while (Numbers.Contains(temp));
+
+                    Numbers[j] = temp;
+                }
+
+
+                Array.Sort(Numbers);
+
+                Console.WriteLine("\n ");
+                for (int k = 0; k < LOTTO; k++)
+                {
+                    Console.Write(Numbers[k] + " ");
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
