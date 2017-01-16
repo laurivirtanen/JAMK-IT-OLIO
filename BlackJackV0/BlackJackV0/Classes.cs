9699a2abf3afd23a4df20e8d539b16082802ee83
lauri virtanen <lavir87@gmail.com>
2017-01-16 17:38:34 +0200

    --

--- BlackJackV0/BlackJackV0/Classes.cs
+++ BlackJackV0/BlackJackV0/Classes.cs
@@ -30,11 +30,118 @@ namespace JAMK.IT
 
     public class Pesukone
     {
-        int[] pesuohjelma = new int[3];
+
+        string[] pesuohjelma = { "Pikapesu - 30min", "Peruspesu - 90min" , "Hienopesu - 150min" };
         public bool IsOn { get; set; }
-        public int MyProperty { get; set; }
+        private int choice { get; set; }
 
 
+        public int ValitseOhjelma()
+        {
+            Console.WriteLine("Tahdotko pestä pyykkiä?(y/n)");
+            if (Console.ReadKey().Key == ConsoleKey.Y) { IsOn = true; } else IsOn = false;
+            if(IsOn==true)
+            {
+                for (int i = 0; i < pesuohjelma.Length; i++)
+                {
+                    Console.Write("\n"+(i+1)+" {0} ", pesuohjelma[i]);
+                }
+                choice = int.Parse(Console.ReadLine())-1;
+                switch (choice)
+                {
+                
+                    default:
+                        Console.WriteLine("Valitsit {0}", pesuohjelma[choice]);
+                        return choice;
+                }
+            }
+            return 0;
+        }
 
     }
+
+    public class Televisio
+    {
+
+        private int channel;
+        public bool IsOn { get; set; }
+        public bool IsRecording { get; set; }
+
+        public int Channel
+        {
+            get { return channel; }
+            set
+            {
+                if (IsOn == true) { channel = value;}
+                else {
+                    Console.Write("\nTV Isn't On\n"); channel = 0;
+                }
+            }
+
+        }
+
+        public bool OnSwitch()
+        { 
+
+
+            Console.WriteLine("Turn the TV on?");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice==1)
+            {
+              return IsOn = true;
+            }
+            else { return IsOn = false; }
+        }
+
+        public int ChangeChannel()
+        {
+            Console.WriteLine("Pick a channel number: ");
+            Channel = int.Parse(Console.ReadLine());
+            Console.WriteLine(Channel);
+            return Channel;
+        }
+    }
+
+    public class Vehicle
+    {
+        private string allData;
+
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        public string AllData{
+            get { return allData; }
+            set { allData += Name + Speed.ToString() + Tyres.ToString(); }
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Auton nimi: {0} Nopeus: {1} Renkaiden lukumäärä: {2}", Name, Speed, Tyres);
+        }
+
+        public string AllDemDatas(string AllData)
+        {
+            
+            Console.WriteLine(AllData);
+            return AllData;
+        }
+    }
+
+    public class Student
+    {
+        public string firstname;
+        public string lastname;
+
+
+        public int Age { get; set; }
+        public float Avg { get; set; }
+
+        public string Name
+        {
+            get { return firstname + " " + lastname; }
+
+        }
+    }
+
 }
--- BlackJackV0/OODemot/KiuasTehtava.cs
+++ BlackJackV0/OODemot/KiuasTehtava.cs
@@ -12,14 +12,61 @@ namespace OODemot
     {
         static void Main(string[] args)
         {
-            JAMK.IT.Kiuas ekaKiuas = new Kiuas();
 
+            List<Student> students = new List<Student>();
+
+            Student student1 = new Student();
+            Student student2 = new Student();
+
+            students.Add(student1);
+            students.Add(student2);
+            student2.Age = 4;
+            student2.Avg = 4.4f;
+            student2.firstname = "Daa";
+            student2.lastname = "Hortensen";
+
+            student1.Age = 5;
+            student1.Avg = 3.4f;
+            student1.firstname = "Lasse";
+            student1.lastname = "Mortensen";
+
+
+            foreach(var Student in students)
+            {
+
+                Console.WriteLine("--Student--");
+                Console.WriteLine(Student.Name);
+                Console.WriteLine(Student.Age);
+                Console.WriteLine(Student.Avg);
+            }
+
+            Vehicle mopedi = new Vehicle();
+
+            mopedi.Name = "Mopo";
+            mopedi.Speed = 25;
+            mopedi.Tyres = 2;
+
+            mopedi.AllData = mopedi.AllData;
+            mopedi.AllDemDatas(mopedi.AllData);
+            mopedi.PrintData();
+            
+
+
+            Pesukone pesukone = new Pesukone();
+            pesukone.IsOn = true;
+            pesukone.ValitseOhjelma();
+
+            Televisio teevee = new Televisio();
+            teevee.OnSwitch();
+            teevee.Channel = 5;
+            teevee.ChangeChannel();
+
+            Kiuas ekaKiuas = new Kiuas();
             ekaKiuas.OnSwitch = true;
             ekaKiuas.Humidity = 15;
             ekaKiuas.Temperature = 70;
-
             Console.WriteLine("Kosteus {0} Lämpötila {1} ", ekaKiuas.Humidity, ekaKiuas.Temperature);
-            JAMK.IT.Start.Main();
+            Start.Main();
 
         }
     }
